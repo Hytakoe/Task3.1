1f00997de74105196a85f94dc7082bb7741e6606
selyu <selyutin.ratmir@yandex.ru>
2024-03-03 20:39:58 +0300

    Almost done

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,14 +1,111 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ContainerAdapter;
 
 public class MainForm extends JFrame {
     private JPanel panel1;
     private JComboBox comboBox1;
-    String[] TrafficLightsTypes = {
-            "Пешеходный",
-            "Автомобильный",
-            "Автомобильный с левой секцией",
-            "Автомобильный с правой секцией",
-            "Автомобильный с обеими секциями",
-    };
-    JComboBox TrafficLights = new JComboBox(TrafficLightsTypes);
+    private JRadioButton RedLight;
+    private JRadioButton YellowLight;
+    private JRadioButton GreenLight;
+    private JRadioButton RightSection;
+    private JRadioButton LeftSection;
+    private JTextField YellowToGreen;
+    private JTextField RedToYellow;
+    private JTextField RedTime;
+    private JTextField GreenTime;
+    private JTextField RedToGreen;
+    private JButton Start;
+    private JButton StartLeft;
+    private JButton StartRight;
+
+    public MainForm() {
+        this.setTitle("FrameMain");
+        this.setContentPane(panel1);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        String[] TrafficLightsTypes = {
+                "Пешеходный",
+                "Автомобильный",
+                "Автомобильный с левой секцией",
+                "Автомобильный с правой секцией",
+                "Автомобильный с обеими секциями",
+        };
+        for (int i = 0; i < TrafficLightsTypes.length; i++) {
+            comboBox1.addItem(TrafficLightsTypes[i]);
+        }
+        TrafficLight currentTL = new TrafficLight();
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = comboBox1.getSelectedIndex();
+                switch (index) {
+                    case 0:
+                        YellowLight.setVisible(false);
+                        RightSection.setVisible(false);
+                        LeftSection.setVisible(false);
+                        RedToGreen.setVisible(true);
+                        RedToYellow.setVisible(false);
+                        YellowToGreen.setVisible(false);
+                        StartLeft.setVisible(false);
+                        StartRight.setVisible(false);
+                        break;
+                    case 1:
+                        YellowLight.setVisible(true);
+                        RightSection.setVisible(false);
+                        LeftSection.setVisible(false);
+                        RedToGreen.setVisible(false);
+                        RedToYellow.setVisible(true);
+                        YellowToGreen.setVisible(true);
+                        StartLeft.setVisible(false);
+                        StartRight.setVisible(false);
+                        break;
+                    case 2:
+                        YellowLight.setVisible(true);
+                        RightSection.setVisible(false);
+                        LeftSection.setVisible(true);
+                        RedToGreen.setVisible(false);
+                        RedToYellow.setVisible(true);
+                        YellowToGreen.setVisible(true);
+                        StartLeft.setVisible(true);
+                        StartRight.setVisible(false);
+                        break;
+                    case 3:
+                        YellowLight.setVisible(true);
+                        RightSection.setVisible(true);
+                        LeftSection.setVisible(false);
+                        RedToGreen.setVisible(false);
+                        RedToYellow.setVisible(true);
+                        YellowToGreen.setVisible(true);
+                        StartLeft.setVisible(false);
+                        StartRight.setVisible(true);
+                        break;
+                    case 4:
+                        YellowLight.setVisible(true);
+                        RightSection.setVisible(true);
+                        LeftSection.setVisible(true);
+                        RedToGreen.setVisible(false);
+                        RedToYellow.setVisible(true);
+                        YellowToGreen.setVisible(true);
+                        StartLeft.setVisible(true);
+                        StartRight.setVisible(true);
+                        break;
+                }
+            }
+        });
+        Start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int type = comboBox1.getSelectedIndex();
+                switch (type) {
+                    case 0:
+                        currentTL.setType(TrafficLightTypes.PEDESTRIAN);
+                        currentTL.setGreenTime(Integer.parseInt(GreenTime.getText()));
+                        currentTL.setRedTime(Integer.parseInt(RedTime.getText()));
+                }
+            }
+        });
+        currentTL.
+    }
 }
--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,5 +1,53 @@
 public class TrafficLight {
     private TrafficLightTypes type;
+    private boolean isRed;
+    public boolean isRed() {
+        return isRed;
+    }
+    public void setRed(boolean red) {
+        isRed = red;
+    }
+
+    private boolean isYellow;
+    public boolean isYellow() {
+        return isYellow;
+    }
+    public void setYellow(boolean yellow) {
+        isYellow = yellow;
+    }
+
+    private boolean isGreen;
+    public boolean isGreen() {
+        return isGreen;
+    }
+    public void setGreen(boolean green) {
+        isGreen = green;
+    }
+
+    private boolean isYellowToGreen;
+    public boolean isYellowToGreen() {
+        return isYellowToGreen;
+    }
+    public void setYellowToGreen(boolean yellowToGreen) {
+        isYellowToGreen = yellowToGreen;
+    }
+
+    private boolean isRedToYellow;
+    public boolean isRedToYellow() {
+        return isRedToYellow;
+    }
+    public void setRedToYellow(boolean redToYellow) {
+        isRedToYellow = redToYellow;
+    }
+
+    private boolean isRedToGreen;
+    public boolean isRedToGreen() {
+        return isRedToGreen;
+    }
+    public void setRedToGreen(boolean redToGreen) {
+        isRedToGreen = redToGreen;
+    }
+
     public TrafficLightTypes getType() {
         return type;
     }
@@ -17,6 +65,19 @@ public class TrafficLight {
     public void setRedToYellowTime(int redToYellowTime) {
         if (!(this.type == TrafficLightTypes.PEDESTRIAN)) {
             this.redToYellowTime = redToYellowTime;
+        }
+    }
+
+    private int redToGreenTime;
+    public int redToGreenTime() {
+        if (this.type == TrafficLightTypes.PEDESTRIAN) {
+            return yellowToGreenTime;
+        }
+        else return 0;
+    }
+    public void redToGreenTime(int yellowToGreenTime) {
+        if (this.type == TrafficLightTypes.PEDESTRIAN) {
+            this.yellowToGreenTime = yellowToGreenTime;
         }
     }
 
@@ -47,5 +108,13 @@ public class TrafficLight {
     }
     public void setGreenTime(int greenTime) {
         this.greenTime = greenTime;
+    }
+    public void startPedestrian() throws InterruptedException {
+        this.setRed(true);
+        Thread.sleep(this.redTime * 1000);
+        this.setRed(false);
+        this.setGreen(true);
+        Thread.sleep(this.greenTime * 1000);
+        this.setGreen(false);
     }
 }
