6a9e597282823a7596e06b418c14e3f67e3dd6af
Hytakoe <selyutin.ratmir@yandex.ru>
2024-03-11 09:42:27 +0300

    Some troubles left

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class TrafficLight {
     private TrafficLightTypes type;
     private boolean isRed;
@@ -80,6 +83,36 @@ public class TrafficLight {
             this.yellowToGreenTime = yellowToGreenTime;
         }
     }
+    public JRadioButton redLight;
+    public JRadioButton yellowLight;
+    public JRadioButton greenLight;
+    public JRadioButton rightLight;
+    public JRadioButton leftLight;
+    private int rightTime;
+    public int getRightTime() {
+        if (this.type == TrafficLightTypes.CAR_ALL || this.type == TrafficLightTypes.CAR_RIGHT) {
+            return rightTime;
+        }
+        else return 0;
+    }
+    public void setRightTime(int rightTime) {
+        if (this.type == TrafficLightTypes.CAR_ALL || this.type == TrafficLightTypes.CAR_RIGHT) {
+            this.rightTime = rightTime;
+        }
+    }
+
+    private int leftTime;
+    public int getleftTime() {
+        if (this.type == TrafficLightTypes.CAR_ALL || this.type == TrafficLightTypes.CAR_LEFT) {
+            return leftTime;
+        }
+        else return 0;
+    }
+    public void setleftTime(int leftTime) {
+        if (this.type == TrafficLightTypes.CAR_ALL || this.type == TrafficLightTypes.CAR_LEFT) {
+            this.leftTime = leftTime;
+        }
+    }
 
     private int yellowToGreenTime;
     public int getYellowToGreenTime() {
@@ -111,10 +144,56 @@ public class TrafficLight {
     }
     public void startPedestrian() throws InterruptedException {
         this.setRed(true);
+        this.redLight.setBackground(Color.red);
         Thread.sleep(this.redTime * 1000);
         this.setRed(false);
+        this.redLight.setBackground(Color.lightGray);
         this.setGreen(true);
+        this.greenLight.setBackground(Color.green);
         Thread.sleep(this.greenTime * 1000);
         this.setGreen(false);
+        this.greenLight.setBackground(Color.lightGray);
+    }
+    public void startAuto() throws InterruptedException {
+        this.setRed(true);
+        this.redLight.setBackground(Color.red);
+        Thread.sleep(this.redTime * 1000);
+        this.setYellow(true);
+        this.yellowLight.setBackground(Color.yellow);
+        Thread.sleep(this.yellowToGreenTime * 1000);
+        this.setRed(false);
+        this.redLight.setBackground(Color.lightGray);
+        this.setYellow(false);
+        this.yellowLight.setBackground(Color.lightGray);
+        this.setGreen(true);
+        this.greenLight.setBackground(Color.green);
+        Thread.sleep(this.greenTime * 1000);
+        this.setGreen(false);
+        for (int i = 0; i < 4; i++){
+            this.greenLight.setBackground(Color.GREEN);
+            Thread.sleep(500);
+            this.greenLight.setBackground(Color.lightGray);
+        }
+        this.yellowLight.setBackground(Color.yellow);
+        Thread.sleep(1000);
+        this.yellowLight.setBackground(Color.lightGray);
+    }
+    public void startRight() throws InterruptedException {
+        this.rightLight.setBackground(Color.green);
+        Thread.sleep(this.rightTime * 1000);
+        for (int i = 0; i < 4; i++){
+            this.rightLight.setBackground(Color.GREEN);
+            Thread.sleep(500);
+            this.rightLight.setBackground(Color.lightGray);
+        }
+    }
+    public void startLeft() throws InterruptedException {
+        this.leftLight.setBackground(Color.green);
+        Thread.sleep(this.leftTime * 1000);
+        for (int i = 0; i < 4; i++){
+            this.leftLight.setBackground(Color.GREEN);
+            Thread.sleep(500);
+            this.leftLight.setBackground(Color.lightGray);
+        }
     }
 }
